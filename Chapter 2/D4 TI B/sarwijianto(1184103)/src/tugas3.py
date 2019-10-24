0825aada65ce3a2775f47b3d37d2b40f6a9f13af
sarwijianto <56343078+sarwijianto@users.noreply.github.com>
2019-10-24 12:16:59 +0700

    Add files via upload

---
+++ Chapter 2/D4 TI B/sarwijianto(1184103)/src/tugas3.py
@@ -0,0 +1,12 @@
+npm = 1184103
+jumlah = 1+0+3
+pms = "103"
+
+print ("1 + 0 + 3 = ", jumlah)
+
+print (" input : ", npm)
+
+print ("output : ")
+
+for i in range (jumlah) :
+    print ("Hello, ", npm, "apa kabar ?")
